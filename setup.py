9477c805e25577107575e4986eebcb8b9742542d
ueshunJP <shun_ueda@dwango.co.jp>
2025-02-07 02:26:27 +0900

    temp

--- setup.py
+++ setup.py
@@ -95,9 +95,6 @@ class CMakeBuild(build_ext):
         build_temp = Path(self.build_temp) / ext.name
         if not build_temp.exists():
             build_temp.mkdir(parents=True)
-        with open("temp.txt", "wt") as fp:
-            for args in cmake_args:
-                fp.write(f"{args}\n")
 
         subprocess.run(["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True)
         subprocess.run(["cmake", "--build", ".", *build_args], cwd=build_temp, check=True)
